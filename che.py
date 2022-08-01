6b4e0f82a1593910928440b7ed9500cee317d061
rootmelo92118 <32770959+rootmelo92118@users.noreply.github.com>
2022-08-01 14:19:54 +0800

    Update che.py

--- che.py
+++ che.py
@@ -13,8 +13,11 @@ def removeInvalidDomain(domain_list,remove_ip=None,domain_type="A",source_port=0
     listB = []
     for i in domain_list:
         dnsres = DNS_Query(i,domain_type,source_port=source_port)
-        if remove_ip in dnsres and remove_ip != None:
-            pass
+        if remove_ip != None:
+            if remove_ip in dnsres:
+                pass
+            else:
+                listB.append(i)
         elif "Error: unable to start thread" in dnsres:
             pass
         else:
