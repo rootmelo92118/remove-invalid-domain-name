39ef6328dc1433bade3a56b2b12fa70ec111c54a
rootmelo92118 <32770959+rootmelo92118@users.noreply.github.com>
2023-12-07 10:59:32 +0800

    Update che.py

--- che.py
+++ che.py
@@ -1,12 +1,12 @@
 import dns.resolver
 
 def DNS_Query(domain_name,domain_type,source_ip=None,source_port=0):
-	try:
-		A = my_resolver.query(domain_name,domain_type,source=source_ip,source_port=source_port)   	
-		for i in A.response.answer:
-			return str(i.to_text())
-	except Exception as e:
-		return str(domain_name + " " + domain_type + " Error: unable to start thread")
+    try:
+        A = my_resolver.query(domain_name,domain_type,source=source_ip,source_port=source_port)
+        for i in A.response.answer:
+            return str(i.to_text())
+    except Exception as e:
+        return str(domain_name + " " + domain_type + " Error: unable to start thread")
 
   
 def removeInvalidDomain(domain_list,remove_invalid_domains=True,remove_ip=None,domain_type=None,source_port=0,reverse_mode=False):
